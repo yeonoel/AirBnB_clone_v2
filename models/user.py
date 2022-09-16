49202222062a9f1c2817a4616129316c94809fd4
yeonoel <yeopevroguinoel@gmail.com>
2022-09-16 10:00:09 +0000

    Correction Task

--- models/user.py
+++ models/user.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """This module defines a class User"""
 
 from os import getenv
@@ -10,29 +9,18 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-
-    __tablename__ = "users"
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-
+    __tablename__ = 'users'
+    if storage_type == 'db':
         email = Column(String(128), nullable=False)
-
         password = Column(String(128), nullable=False)
-
         first_name = Column(String(128), nullable=True)
-
         last_name = Column(String(128), nullable=True)
-
-        places = relationship("Place", cascade="all, delete", backref="user")
-
-        review = relationship("Review", cascade="all, delete", backref="user")
-
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
     else:
-
-        email = ''
-
-        password = ''
-
-        first_name = ''
-
-        last_name = ''
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
