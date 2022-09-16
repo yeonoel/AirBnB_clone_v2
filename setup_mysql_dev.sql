a24ac17ff51b9e93abb058364338c3e335671cd2
yeonoel <yeopevroguinoel@gmail.com>
2022-09-16 09:46:45 +0000

    Up to date

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@
 -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,9 +4,9 @@
 -- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
 -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost'
 FLUSH PRIVILEGES;
