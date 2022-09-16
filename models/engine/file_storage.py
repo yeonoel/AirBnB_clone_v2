0f3dbf1513eabf3ac2eda06e4e62bba4064215cf
yeonoel <yeopevroguinoel@gmail.com>
2022-09-16 11:01:38 +0000

    Up date Task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,113 +6,69 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-
     __file_path = 'file.json'
-
     __objects = {}
 
     def all(self, cls=None):
-
         """Returns a dictionary of models currently in storage"""
-
-        if cls is not None:
-
-            cls_objects = {}
-
-            for k, v in FileStorage.__objects.items():
-
-                if k.startswith(cls.__name__):
-
-                    cls_objects.update({k: v})
-
-            return cls_objects
-
-        else:
-
-            return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        cls_name = cls.__name__
+        dct = {}
+        for key in self.__objects.keys():
+            if key.split('.')[0] == cls_name:
+                dct[key] = self.__objects[key]
+        return dct
 
     def new(self, obj):
-
         """Adds new object to storage dictionary"""
-
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.__objects.update(
+            {obj.to_dict()['__class__'] + '.' + obj.id: obj}
+            )
 
     def save(self):
-
         """Saves storage dictionary to file"""
-
-        with open(FileStorage.__file_path, 'w') as f:
-
+        with open(self.__file_path, 'w') as f:
             temp = {}
-
-            temp.update(FileStorage.__objects)
-
+            temp.update(self.__objects)
             for key, val in temp.items():
-
                 temp[key] = val.to_dict()
-
-            json.dump(temp, f, sort_keys=True, indent=4)
-
-    def delete(self, obj=None):
-
-        """Deletes the object obj if obj is in __objects"""
-
-        if obj is not None:
-
-            for k, v in FileStorage.__objects.items():
-
-                if v is obj:
-
-                    tmp = k
-
-            FileStorage.__objects.pop(tmp)
+            json.dump(temp, f)
 
     def reload(self):
-
         """Loads storage dictionary from file"""
-
         from models.base_model import BaseModel
-
         from models.user import User
-
         from models.place import Place
-
         from models.state import State
-
         from models.city import City
-
         from models.amenity import Amenity
-
         from models.review import Review
 
         classes = {
-
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-
-            'State': State, 'City': City, 'Amenity': Amenity,
-
-            'Review': Review
-
-        }
-
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
-
             temp = {}
-
-            with open(FileStorage.__file_path, 'r') as f:
-
+            with open(self.__file_path, 'r') as f:
                 temp = json.load(f)
-
                 for key, val in temp.items():
-
                     self.all()[key] = classes[val['__class__']](**val)
-
         except FileNotFoundError:
-
             pass
 
+    def delete(self, obj=None):
+        ''' deletes the object obj from the attribute
+            __objects if it's inside it
+        '''
+        if obj is None:
+            return
+        obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+        if obj_key in self.__objects.keys():
+            del self.__objects[obj_key]
+
     def close(self):
-
-        """Handles storage close"""
-
+        """Call the reload method"""
         self.reload()
