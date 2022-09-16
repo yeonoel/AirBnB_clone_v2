fd408d5a680d7ca4c7cfde46e04ea4c6ce9a6086
yeonoel <yeopevroguinoel@gmail.com>
2022-09-16 09:39:56 +0000

    Up to date

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,17 +1,12 @@
--- Prepares a MySQL server for the hbnb project.
-
-
-
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
-
-USE `hbnb_test_db`;
-
-
-
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost'
-
-	IDENTIFIED BY 'hbnb_test_pwd';
-
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost'
-
-	IDENTIFIED BY 'hbnb_test_pwd';
+-- script that prepares a MySQL server for the project:
+-- A new user hbnb_dev (in localhost)
+-- The password of hbnb_dev should be set to hbnb_dev_pwd
+-- hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+-- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+-- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+FLUSH PRIVILEGES;
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
+FLUSH PRIVILEGES;
