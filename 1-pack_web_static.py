43d0cc273077e915bb32308942d634302b2c15de
yeonoel <yeopevroguinoel@gmail.com>
2022-09-29 20:29:20 +0000

    Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# This script generates a .tgz
+# archive from the contents of the web_static folder
+
+from fabric.api import local
+from datetime import datetime
+
+from fabric.decoration import runs_once
+
+
+@runs_once
+def do_pack():
+    '''generates .tgz archive from the contents of the web_static folder'''
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tar -cvzf {} web_static"
+                   .format(path))
+
+    if result.failed:
+        return None
+    return path
