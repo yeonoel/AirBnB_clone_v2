657eb6d37b89ef52fc1d4d372386943e836eeaf3
yeonoel <yeopevroguinoel@gmail.com>
2022-10-16 13:19:52 +0000

    This script starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Return string to the root /."""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_h():
+    """Return a string to a root /hbnb."""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """ Return string following by value of text."""
+    return ("C " + text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
