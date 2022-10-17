1d9411fd92b115d719260308b9965f3fc64b073d
yeonoel <yeopevroguinoel@gmail.com>
2022-10-17 11:26:06 +0000

    Correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""This script starts a Flask web application."""
-
-from flask import Flask, render_template
+"""Starts a flask app
+    listens to 0.0.0.0:5000
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """a HTML page template at the route /states_list."""
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """remove the current SQLAlchemy Session."""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
